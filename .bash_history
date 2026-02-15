9b133cf436caced6783dddd39a9ebfb93eb981bb
“Superhans79” <s.gilling79.com>
2026-02-15 08:52:05 +0000

    Initial blockchain node on Raspberry Pi

---
+++ .bash_history
@@ -0,0 +1,61 @@
+mkdir blockchain_pi
+cd blockchain_pi
+pwd
+nano blockchain.py
+python3 blockchain.py
+pip install flask
+sudo apt install python3-venv python3-full
+cd ~/blockchain
+ls
+cd ~/blockchain_pi
+ls
+python3 -m venv venv
+source venv/bin/activate
+pip install flask
+source venv/bin/activate
+python3 blockchain.py
+ls
+cd ~/blockchain
+source venv/bin/activate
+nano blockchain.py
+python3 -m venv venv
+ls
+cd blockchain_pi
+ls
+source venv/bin/activate
+pip list
+python3 blockchain.py
+cd ~
+cp -r blockchain blockchain_node2
+exit
+cd ~
+cp -r blockchain blockchain_node2
+ls
+cp -r blockchain_pi blockchain_node2
+~/blockchain
+~/blockchain_node2
+ls
+cd ~/blockchain_node2
+python3 -m venv venv
+source venv/bin/activate
+pip install flask
+git --version
+git config --global user.name "Your Name"
+git config --global user.email "s.gilling79.com"
+close
+end
+deactivate
+git config --global user.name “Superhans79”
+git config --global user.email “s.gilling79@googlemail.com"
+exit
+x
+≈
+√
+off
+git --version
+git config --global user.name “Superhans79”
+git config --global user.email “s.gilling79@googlemail.com"
+git init
+exit
+
+end
---
+++ blockchain_pi/blockchain.py
@@ -0,0 +1,132 @@
+from flask import Flask, jsonify, request
+import hashlib
+import datetime
+import json
+
+class Blockchain:
+
+    def __init__(self):
+        self.chain = []
+        self.transactions = []
+        self.create_block(proof=1, previous_hash='0')
+
+    def create_block(self, proof, previous_hash):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': str(datetime.datetime.now()),
+            'proof': proof,
+            'previous_hash': previous_hash,
+            'transactions': self.transactions
+        }
+        self.transactions = []
+        self.chain.append(block)
+        return block
+
+    def get_previous_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        while True:
+            hash_operation = hashlib.sha256(
+                str(new_proof**2 - previous_proof**2).encode()
+            ).hexdigest()
+            if hash_operation[:4] == '0000':
+                return new_proof
+            new_proof += 1
+
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+
+    def add_transaction(self, sender, receiver, amount):
+        self.transactions.append({
+            'sender': sender,
+            'receiver': receiver,
+            'amount': amount
+        })
+        previous_block = self.get_previous_block()
+        return previous_block['index'] + 1
+
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+
+        while block_index < len(chain):
+            block = chain[block_index]
+
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+
+            hash_operation = hashlib.sha256(
+                str(proof**2 - previous_proof**2).encode()
+            ).hexdigest()
+
+            if hash_operation[:4] != '0000':
+                return False
+
+            previous_block = block
+            block_index += 1
+
+        return True
+
+# Web app
+app = Flask(__name__)
+blockchain = Blockchain()
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    proof = blockchain.proof_of_work(previous_block['proof'])
+    previous_hash = blockchain.hash(previous_block)
+
+    blockchain.add_transaction(
+        sender="Network",
+        receiver="Your Wallet",
+        amount=1
+    )
+
+    block = blockchain.create_block(proof, previous_hash)
+
+    return jsonify({
+        'message': 'Block mined!',
+        'block': block
+    }), 200
+
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    return jsonify({
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }), 200
+
+@app.route('/add_transaction', methods=['POST'])
+def add_transaction():
+    data = request.get_json()
+
+    required = ['sender', 'receiver', 'amount']
+    if not all(k in data for k in required):
+        return 'Missing values', 400
+
+    index = blockchain.add_transaction(
+        data['sender'],
+        data['receiver'],
+        data['amount']
+    )
+
+    return jsonify({
+        'message': f'Transaction will be added to block {index}'
+    }), 201
+
+@app.route('/is_valid', methods=['GET'])
+def is_valid():
+    valid = blockchain.is_chain_valid(blockchain.chain)
+
+    return jsonify({
+        'valid': valid
+    }), 200
+
+app.run(host='0.0.0.0', port=5000)
